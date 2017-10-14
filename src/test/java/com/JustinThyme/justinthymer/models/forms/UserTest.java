615e522d4f1abb49dee5203b36befa736cf340c9
Eric Andrew Chunn <ericandrewchunn@gmail.com>
2017-10-14 13:15:02 -0700

    added empty UserTest.java, plus files generated by previous Create Test

---
+++ src/test/java/com/JustinThyme/justinthymer/models/forms/UserTest.java
@@ -0,0 +1,50 @@
+package com.JustinThyme.justinthymer.models.forms;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void getPhoneNumber() {
+    }
+
+    @Test
+    void setPhoneNumber() {
+    }
+
+    @Test
+    void getArea() {
+    }
+
+    @Test
+    void setArea() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+}
